9302a620d4ac609542f3bb818aebb7ceaa7cd6ed
miroslavangelov <miroslav@devision.com>
2018-12-31 16:26:02 +0200

    remove unused class

--- DatabasesAdvancedHibernate/SpringDataAdvancedQuering/BookshopSystem/src/main/java/app/repository/BookRepository.java
+++ DatabasesAdvancedHibernate/SpringDataAdvancedQuering/BookshopSystem/src/main/java/app/repository/BookRepository.java
@@ -5,7 +5,6 @@ import app.domain.entities.Book;
 import app.domain.entities.EditionType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
